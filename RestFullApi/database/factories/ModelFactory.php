b1ade6ee6bf501ce80205554a795675af5ae7534
amien020596 <offdevamienk@gmail.com>
2020-01-05 22:42:37 +0700

    update (generate factory for transaction)

--- RestFullApi/database/factories/ModelFactory.php
+++ RestFullApi/database/factories/ModelFactory.php
@@ -15,6 +15,8 @@
 
 use App\Category;
 use App\Product;
+use App\Seller;
+use App\Transaction;
 use App\User;
 
 $factory->define(User::class, function (Faker\Generator $faker) {
@@ -46,5 +48,17 @@ $factory->define(Product::class, function (Faker\Generator $faker) {
         'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
         'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
         'seller_id' => User::all()->random()->id,
+        // User::inRandomOrder()->first()->id
+    ];
+});
+
+$factory->define(Transaction::class, function (Faker\generator $faker) {
+    $seller = Seller::has('products')->get()->random();
+    $buyer = User::all()->except($seller->id)->random();
+
+    return [
+        'quantity' => $faker->numberBetween(1, 3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
     ];
 });
