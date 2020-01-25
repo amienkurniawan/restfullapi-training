3c5ab9fe9552491fefe6d72b2bb31e491b9f07f4
amien020596 <offdevamienk@gmail.com>
2020-01-25 13:29:19 +0700

    update (add Mail: UserMailChanged )

--- RestFullApi/app/User.php
+++ RestFullApi/app/User.php
@@ -24,7 +24,13 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'verified', 'verification_token', 'admin', 'deleted_at'
+        'name',
+        'email',
+        'password',
+        'verified',
+        'verification_token',
+        'admin',
+        'deleted_at'
     ];
 
     /**
