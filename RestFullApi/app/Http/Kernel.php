b0a350e4d9d7feb872042e913cf7752cbc5bd5cf
amien020596 <offdevamienk@gmail.com>
2020-02-12 21:38:56 +0700

    update (add token lifetimes)

--- RestFullApi/app/Http/Kernel.php
+++ RestFullApi/app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Laravel\Passport\Http\Middleware\CheckClientCredentials;
 
 class Kernel extends HttpKernel
 {
@@ -60,5 +61,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'signature' => \App\Http\Middleware\SignatureMiddleware::class,
         'transformInput' => \App\Http\Middleware\TransformInput::class,
+        'client.credentials' => CheckClientCredentials::class,
     ];
 }
